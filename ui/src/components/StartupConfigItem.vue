a62b7803583b0f71a9d6f754d600318e5ac53f58
Stefan Hausotte <stefan.hausotte@gmx.de>
2023-11-17 22:00:25 +0100

    improve UI of settings

--- ui/src/components/StartupConfigItem.vue
+++ ui/src/components/StartupConfigItem.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="configItem">
-    <!-- table header -->
-    <span class="headerToml">config.toml</span>
-    <span class="headerEnv">env. var.</span>
-    <span class="headerValue">value</span>
-    <!-- table body -->
     <span class="tomlValue">
       <div>[{{ props.tomlTable }}]</div>
       <div>{{ props.toml }}</div>
@@ -27,42 +22,20 @@ const props = defineProps<{
 .configItem {
   border-bottom-style: solid;
   border-bottom-width: 0.01rem;
-  padding: 0.5rem;
+  padding: 0.5rem 0 0.5rem 0;
   display: grid;
   grid-template-columns: 1fr 2fr 1fr;
-  grid-template-rows: auto auto;
-}
-
-.headerToml {
-  grid-column: 1;
-  grid-row: 1;
-  font-size: smaller;
-}
-
-.headerEnv {
-  grid-column: 2;
-  grid-row: 1;
-  font-size: smaller;
-}
-
-.headerValue {
-  grid-column: 3;
-  grid-row: 1;
-  font-size: smaller;
 }
 
 .tomlValue {
   grid-column: 1;
-  grid-row: 2;
 }
 
 .nameEnv {
   grid-column: 2;
-  grid-row: 2;
 }
 
 .value {
   grid-column: 3;
-  grid-row: 2;
 }
 </style>
