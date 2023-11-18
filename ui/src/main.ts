8b8f33cc9cfe89fb1c19e373a9e419b5f50d8ebe
Stefan Hausotte <stefan.hausotte@gmx.de>
2023-11-18 16:10:03 +0100

    try to fix ui

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './views/App.vue'
 // @ts-ignore
 import VueHighlightJS from 'vue3-highlightjs' // https://www.npmjs.com/package/vue3-highlightjs
 import Axios from 'axios'
 import { setupCache } from 'axios-cache-interceptor';
 import VueAxios from 'vue-axios'
-import {store} from "@/store/store";
-import router from "@/router";
+import {store} from "./store/store";
+import router from "./router";
 
 import 'highlight.js/styles/default.css'
 import './assets/css/main.css'
--- ui/src/remote-routes.ts
+++ ui/src/remote-routes.ts
@@ -3,26 +3,26 @@ export function kellnr_url(path: string): string {
   return new URL(path, BASE_URL).toString();
 }
 
-export const ADD_TOKEN = kellnr_url("/user/add_token");
-export const DELETE_TOKEN = (id: number) => kellnr_url(`/user/delete_token/${id}`);
-export const LIST_TOKENS = kellnr_url("/user/list_tokens");
-export const CHANGE_PWD = kellnr_url("/user/changepwd");
-export const LOGIN_STATE = kellnr_url("/user/login_state");
-export const LOGOUT = kellnr_url("/user/logout");
-export const ADD_USER = kellnr_url("/user/add");
-export const DELETE_USER = (name: string) => kellnr_url(`/user/delete/${name}`);
-export const LIST_USERS = kellnr_url("/user/list_users");
-export const RESET_PWD = (name: string) => kellnr_url(`/user/resetpwd/${name}`);
-export const LOGIN = kellnr_url("/user/login");
+export const ADD_TOKEN = kellnr_url("/api/v1/user/add_token");
+export const DELETE_TOKEN = (id: number) => kellnr_url(`/api/v1/user/delete_token/${id}`);
+export const LIST_TOKENS = kellnr_url("/api/v1/user/list_tokens");
+export const CHANGE_PWD = kellnr_url("/api/v1/user/changepwd");
+export const LOGIN_STATE = kellnr_url("/api/v1/user/login_state");
+export const LOGOUT = kellnr_url("/api/v1/user/logout");
+export const ADD_USER = kellnr_url("/api/v1/user/add");
+export const DELETE_USER = (name: string) => kellnr_url(`/api/v1/user/delete/${name}`);
+export const LIST_USERS = kellnr_url("/api/v1/user/list_users");
+export const RESET_PWD = (name: string) => kellnr_url(`/api/v1/user/resetpwd/${name}`);
+export const LOGIN = kellnr_url("/api/v1/user/login");
 
-export const CRATE_DATA = kellnr_url("/crate_data");
-export const CRATESIO_DATA = kellnr_url("/cratesio_data");
-export const CRATES = kellnr_url("/crates");
-export const CRATE_DELETE = kellnr_url("/delete_crate");
-export const VERSION = kellnr_url("/version");
-export const SETTINGS = kellnr_url("/settings");
-export const STATISTICS = kellnr_url("/statistic");
-export const SEARCH = kellnr_url("/search");
+export const CRATE_DATA = kellnr_url("/api/v1/ui/crate_data");
+export const CRATESIO_DATA = kellnr_url("/api/v1/ui/cratesio_data");
+export const CRATES = kellnr_url("/api/v1/ui/crates");
+export const CRATE_DELETE = kellnr_url("/api/v1/ui/delete_crate");
+export const VERSION = kellnr_url("/api/v1/ui/version");
+export const SETTINGS = kellnr_url("/api/v1/ui/settings");
+export const STATISTICS = kellnr_url("/api/v1/ui/statistic");
+export const SEARCH = kellnr_url("/api/v1/ui/search");
 
 export const CRATES_IO_INDEX = kellnr_url("/api/v1/cratesio/index");
 export const DOCS_BUILD = kellnr_url("/api/v1/docs/build");
