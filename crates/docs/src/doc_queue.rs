a9c76c6e568ddb50932b79361be00fb81a98e66a
mbodmer <marc.bodmer@email.ch>
2024-03-28 19:47:53 +0100

    build docs with all features enabled
    Signed-off-by: mbodmer <marc.bodmer@email.ch>

--- crates/docs/src/doc_queue.rs
+++ crates/docs/src/doc_queue.rs
@@ -1,6 +1,6 @@
 use crate::compute_doc_url;
 use cargo::{
-    core::Workspace,
+    core::{resolver::CliFeatures, Workspace},
     ops::{self, CompileOptions, DocOptions},
     util::command_prelude::CompileMode,
     CargoResult, Config,
@@ -104,7 +104,10 @@ fn generate_docs(crate_path: impl AsRef<Path>) -> CargoResult<()> {
     let manifest_path = crate_path.as_ref().join("Cargo.toml").canonicalize()?;
     let config = Config::default()?;
     let workspace = Workspace::new(&manifest_path, &config)?;
-    let compile_opts = CompileOptions::new(&config, CompileMode::Doc { deps: false })?;
+    let compile_opts = CompileOptions {
+        cli_features: CliFeatures::new_all(true),
+        ..CompileOptions::new(&config, CompileMode::Doc { deps: false })?
+    };
     let options = DocOptions {
         open_result: false,
         compile_opts,
