c031163b814e92e007230407a3efc3b6951a3b8d
5Dev24 <omega524dev@gmail.com>
2025-02-24 13:36:34 -0500

    Add required_crate_fields to UI

--- ui/src/components/StartupConfig.vue
+++ ui/src/components/StartupConfig.vue
@@ -21,6 +21,8 @@
       :value="settings.registry.max_db_connections"></startup-config-item>
     <startup-config-item tomlTable="registry" toml="auth_required" env="KELLNR_REGISTRY__AUTH_REQUIRED"
       :value="settings.registry.auth_required"></startup-config-item>
+    <startup-config-item tomlTable="registry" toml="required_crate_fields" env="KELLNR_REGISTRY__REQUIRED_CRATE_FIELDS"
+      :value="settings.registry.required_crate_fields"></startup-config-item>
   </div>
 
   <div class="settingsSection">
