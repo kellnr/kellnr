38c7d268b39ab4eb396fe3abfa94f3df9750f74f
Stefan Hausotte <stefan.hausotte@gmx.de>
2025-04-15 12:03:02 +0200

    remove bulma

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -10,8 +10,6 @@ import router from "./router";
 
 import 'highlight.js/styles/default.css'
 import './assets/css/main.css'
-import '../node_modules/bulma/css/bulma.min.css'
-import '../node_modules/bulma-switch/dist/css/bulma-switch.min.css'
 import '../node_modules/@fortawesome/fontawesome-free/js/all'
 
 const axios = setupCache(Axios);
