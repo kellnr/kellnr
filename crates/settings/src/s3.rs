16662ae95e1a545bf95d9b38d5c6cdfc18a8581b
Sean Bentley <seanbentley@rivianvw.tech>
2025-10-20 23:31:09 -0700

    put custom impl Default back on S3, adding Some() where needed

--- crates/settings/src/s3.rs
+++ crates/settings/src/s3.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Default, Deserialize, Serialize, Eq, PartialEq, Clone)]
+#[derive(Debug, Deserialize, Serialize, Eq, PartialEq, Clone)]
 pub struct S3 {
     pub enabled: bool,
     pub access_key: Option<String>,
@@ -10,4 +10,19 @@ pub struct S3 {
     pub allow_http: bool,
     pub crates_bucket: Option<String>,
     pub cratesio_bucket: Option<String>,
+}
+
+impl Default for S3 {
+    fn default() -> Self {
+        Self {
+            enabled: false,
+            access_key: Some("minioadmin".to_string()),
+            secret_key: Some("minioadmin".to_string()),
+            region: Some("us-east-1".to_string()),
+            endpoint: Some("http://localhost:9000/".to_string()),
+            allow_http: true,
+            crates_bucket: Some("kellnr-crates".to_string()),
+            cratesio_bucket: Some("kellnr-cratesio".to_string()),
+        }
+    }
 }
